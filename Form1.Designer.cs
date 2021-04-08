565a88c153a24af6d4ceb445d588ab1c9f9dc9e7
Ozzy137 <ozzman137@gmail.com>
2021-04-07 20:50:41 -0700

    Project 2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,18 +39,23 @@
             this.btnDisp = new System.Windows.Forms.Button();
             this.lstFrsArr = new System.Windows.Forms.ListBox();
             this.lstSecArr = new System.Windows.Forms.ListBox();
+            this.lblArrVal = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtBxFstArr
             // 
-            this.txtBxFstArr.Location = new System.Drawing.Point(166, 122);
+            this.txtBxFstArr.BackColor = System.Drawing.Color.PapayaWhip;
+            this.txtBxFstArr.ForeColor = System.Drawing.Color.Maroon;
+            this.txtBxFstArr.Location = new System.Drawing.Point(166, 159);
             this.txtBxFstArr.Name = "txtBxFstArr";
             this.txtBxFstArr.Size = new System.Drawing.Size(79, 22);
             this.txtBxFstArr.TabIndex = 0;
             // 
             // txtBxSecArr
             // 
-            this.txtBxSecArr.Location = new System.Drawing.Point(460, 122);
+            this.txtBxSecArr.BackColor = System.Drawing.Color.PapayaWhip;
+            this.txtBxSecArr.ForeColor = System.Drawing.Color.Maroon;
+            this.txtBxSecArr.Location = new System.Drawing.Point(460, 158);
             this.txtBxSecArr.Name = "txtBxSecArr";
             this.txtBxSecArr.Size = new System.Drawing.Size(75, 22);
             this.txtBxSecArr.TabIndex = 1;
@@ -58,8 +63,10 @@
             // lblFstArr
             // 
             this.lblFstArr.AutoSize = true;
+            this.lblFstArr.BackColor = System.Drawing.Color.PapayaWhip;
             this.lblFstArr.Font = new System.Drawing.Font("Calisto MT", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFstArr.Location = new System.Drawing.Point(72, 122);
+            this.lblFstArr.ForeColor = System.Drawing.Color.Maroon;
+            this.lblFstArr.Location = new System.Drawing.Point(57, 158);
             this.lblFstArr.Name = "lblFstArr";
             this.lblFstArr.Size = new System.Drawing.Size(88, 22);
             this.lblFstArr.TabIndex = 2;
@@ -68,8 +75,10 @@
             // lblSecArr
             // 
             this.lblSecArr.AutoSize = true;
+            this.lblSecArr.BackColor = System.Drawing.Color.PapayaWhip;
             this.lblSecArr.Font = new System.Drawing.Font("Calisto MT", 10.8F, System.Drawing.FontStyle.Bold);
-            this.lblSecArr.Location = new System.Drawing.Point(366, 121);
+            this.lblSecArr.ForeColor = System.Drawing.Color.Maroon;
+            this.lblSecArr.Location = new System.Drawing.Point(357, 157);
             this.lblSecArr.Name = "lblSecArr";
             this.lblSecArr.Size = new System.Drawing.Size(88, 22);
             this.lblSecArr.TabIndex = 3;
@@ -78,7 +87,9 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.PapayaWhip;
             this.label3.Font = new System.Drawing.Font("Orbitron", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Maroon;
             this.label3.Location = new System.Drawing.Point(215, 36);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(349, 52);
@@ -88,67 +99,96 @@
             // btnAdd
             // 
             this.btnAdd.AutoSize = true;
-            this.btnAdd.Location = new System.Drawing.Point(202, 281);
+            this.btnAdd.BackColor = System.Drawing.Color.PapayaWhip;
+            this.btnAdd.ForeColor = System.Drawing.Color.Maroon;
+            this.btnAdd.Location = new System.Drawing.Point(318, 338);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(118, 39);
             this.btnAdd.TabIndex = 5;
             this.btnAdd.Text = "Add Element";
-            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.UseVisualStyleBackColor = false;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnClear
             // 
             this.btnClear.AutoSize = true;
-            this.btnClear.Location = new System.Drawing.Point(326, 281);
+            this.btnClear.BackColor = System.Drawing.Color.PapayaWhip;
+            this.btnClear.ForeColor = System.Drawing.Color.Maroon;
+            this.btnClear.Location = new System.Drawing.Point(347, 417);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(89, 38);
             this.btnClear.TabIndex = 6;
             this.btnClear.Text = "Clear";
-            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // btnReset
             // 
             this.btnReset.AutoSize = true;
-            this.btnReset.Location = new System.Drawing.Point(442, 280);
+            this.btnReset.BackColor = System.Drawing.Color.PapayaWhip;
+            this.btnReset.ForeColor = System.Drawing.Color.Maroon;
+            this.btnReset.Location = new System.Drawing.Point(460, 417);
             this.btnReset.Name = "btnReset";
             this.btnReset.Size = new System.Drawing.Size(93, 39);
             this.btnReset.TabIndex = 7;
             this.btnReset.Text = "Reset Array";
-            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
             // 
             // btnDisp
             // 
-            this.btnDisp.Location = new System.Drawing.Point(202, 345);
+            this.btnDisp.BackColor = System.Drawing.Color.PapayaWhip;
+            this.btnDisp.ForeColor = System.Drawing.Color.Maroon;
+            this.btnDisp.Location = new System.Drawing.Point(202, 416);
             this.btnDisp.Name = "btnDisp";
             this.btnDisp.Size = new System.Drawing.Size(118, 40);
             this.btnDisp.TabIndex = 8;
             this.btnDisp.Text = "Display Arrays";
-            this.btnDisp.UseVisualStyleBackColor = true;
+            this.btnDisp.UseVisualStyleBackColor = false;
             this.btnDisp.Click += new System.EventHandler(this.btnDisp_Click);
             // 
             // lstFrsArr
             // 
+            this.lstFrsArr.BackColor = System.Drawing.Color.PapayaWhip;
+            this.lstFrsArr.ForeColor = System.Drawing.Color.Maroon;
             this.lstFrsArr.FormattingEnabled = true;
             this.lstFrsArr.ItemHeight = 16;
-            this.lstFrsArr.Location = new System.Drawing.Point(170, 159);
+            this.lstFrsArr.Location = new System.Drawing.Point(170, 225);
             this.lstFrsArr.Name = "lstFrsArr";
             this.lstFrsArr.Size = new System.Drawing.Size(75, 84);
             this.lstFrsArr.TabIndex = 9;
             // 
             // lstSecArr
             // 
+            this.lstSecArr.BackColor = System.Drawing.Color.PapayaWhip;
+            this.lstSecArr.ForeColor = System.Drawing.Color.Maroon;
             this.lstSecArr.FormattingEnabled = true;
             this.lstSecArr.ItemHeight = 16;
-            this.lstSecArr.Location = new System.Drawing.Point(460, 159);
+            this.lstSecArr.Location = new System.Drawing.Point(460, 225);
             this.lstSecArr.Name = "lstSecArr";
             this.lstSecArr.Size = new System.Drawing.Size(75, 84);
             this.lstSecArr.TabIndex = 10;
+            // 
+            // lblArrVal
+            // 
+            this.lblArrVal.AutoSize = true;
+            this.lblArrVal.BackColor = System.Drawing.Color.PapayaWhip;
+            this.lblArrVal.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArrVal.ForeColor = System.Drawing.Color.Maroon;
+            this.lblArrVal.Location = new System.Drawing.Point(557, 111);
+            this.lblArrVal.Name = "lblArrVal";
+            this.lblArrVal.Size = new System.Drawing.Size(114, 23);
+            this.lblArrVal.TabIndex = 11;
+            this.lblArrVal.Text = "Array Values";
+            this.lblArrVal.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(776, 710);
+            this.Controls.Add(this.lblArrVal);
             this.Controls.Add(this.lstSecArr);
             this.Controls.Add(this.lstFrsArr);
             this.Controls.Add(this.btnDisp);
@@ -181,6 +221,7 @@
         private System.Windows.Forms.Button btnDisp;
         private System.Windows.Forms.ListBox lstFrsArr;
         private System.Windows.Forms.ListBox lstSecArr;
+        private System.Windows.Forms.Label lblArrVal;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Drawing.Text;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,7 @@ namespace Assignment_2_Arrays_APP
         private int[] firstArray = new int[MAX_SIZE];
         private int[] secondArray = new int[MAX_SIZE];
         private int[] thirdArray = new int[MAX_SIZE];
+        private string[] allNumbers = new string[MAX_SIZE];
         private int i = 0;
         private int count = 0;
         public Form1()
@@ -32,7 +34,7 @@ namespace Assignment_2_Arrays_APP
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            
+
 
             // Tests whether both textboxes have values
             if (txtBxFstArr.Text == "" || txtBxSecArr.Text == "")
@@ -56,23 +58,39 @@ namespace Assignment_2_Arrays_APP
                     // Adds the user input values to the arrays
                     if (count <= 4)
                     {
-                        int numOne = Convert.ToInt32(txtBxFstArr.Text);
-                        int numTwo = Convert.ToInt32(txtBxSecArr.Text);
+                        try
+                        {
+                            int numOne = Convert.ToInt32(txtBxFstArr.Text);
+                            int numTwo = Convert.ToInt32(txtBxSecArr.Text);
+                            firstArray[i] = numOne;
+                            secondArray[i] = numTwo;
 
-                        firstArray[i] = numOne;
-                        secondArray[i] = numTwo;
+                            lstFrsArr.Items.Add(txtBxFstArr.Text);
+                            lstSecArr.Items.Add(txtBxSecArr.Text);
 
-                        lstFrsArr.Items.Add(txtBxFstArr.Text);
-                        lstSecArr.Items.Add(txtBxSecArr.Text);
 
-                        ++count; 
-                        i++;
+                            thirdArray[i] = firstArray[i] * secondArray[i];
 
+                            ++count;
+                            i++;
+
+                        }
+                        catch (FormatException err)
+                        {
+                            MessageBox.Show(err.Message, "Waring!");
+                        }
+                        catch(OverflowException Ove)
+                        {
+                            MessageBox.Show(Ove.Message, "Warning");
+                        }
+                        catch(IndexOutOfRangeException rang)
+                        {
+                            MessageBox.Show(rang.Message, "Warning");
+                        }
                         txtBxFstArr.Clear();
                         txtBxSecArr.Clear();
 
-                        int numMult = firstArray[i] * secondArray[i];
-                        thirdArray[i] = numMult;
+
                     }
                     else
                     {
@@ -81,24 +99,44 @@ namespace Assignment_2_Arrays_APP
                         txtBxFstArr.Clear();
                         txtBxSecArr.Clear();
                     }
-                    
+
                 }
+                txtBxFstArr.Focus();
             }
         }
 
         private void btnDisp_Click(object sender, EventArgs e)
         {
-            string firstArr = "";
-            string secondArr = "";
-            string thirdArr = "";
-            for (i = 0; i <= 4; i++)
+            for (int h = 0; h <= 4; h++)
             {
-                 firstArr = Convert.ToString(firstArray[i]);
-                 secondArr = Convert.ToString(secondArray[i]);
-                 thirdArr = Convert.ToString(thirdArray[i]);
-
+                allNumbers[h] = "\n\n" + "First Array Values: " + Convert.ToString(firstArray[h]) + "\n" + "Second Array: " + Convert.ToString(secondArray[h]) + "\n" + "Third Array: " + Convert.ToString(thirdArray[h]) + "\n";
+                lblArrVal.Text += allNumbers[h];
             }
-            MessageBox.Show("First Array Values: " + firstArr+ "/n" +)
+            lblArrVal.Visible = true;
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtBxFstArr.Clear();
+            txtBxSecArr.Clear();
+            txtBxFstArr.Focus();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            i = 0;
+            count = 0;
+            lblArrVal.Visible = false;
+            txtBxFstArr.Clear();
+            txtBxSecArr.Clear();
+            txtBxFstArr.Focus();
+            lstFrsArr.Items.Clear();
+            lstSecArr.Items.Clear();
+            Array.Clear(firstArray, 0, firstArray.Length);
+            Array.Clear(secondArray, 0, secondArray.Length);
+            Array.Clear(thirdArray, 0, thirdArray.Length);
+            lblArrVal.Text = "Array Values";
         }
     }
 }
